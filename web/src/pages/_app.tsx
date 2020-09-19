97a20e23866a55aa5f02a193be6fad5c3b1f609a
Markol17 <markolivierpoulin@hotmail.com>
2020-09-18 20:39:10 -0400

    fixed rendering issues

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,9 +3,10 @@ import { useEffect } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../theme';
+import Head from 'next/head';
 
 function CoLab({ Component, pageProps }: any) {
-  useEffect(() => {
+  React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
@@ -14,10 +15,19 @@ function CoLab({ Component, pageProps }: any) {
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>CoLab</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
