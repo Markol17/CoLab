048acbd6db0a5018377476d523b0bd3a153d8758
Markol17 <markolivierpoulin@hotmail.com>
2020-10-26 14:41:56 -0400

    fixed scrollbar

--- web/src/components/drawers/IndexDrawer.tsx
+++ web/src/components/drawers/IndexDrawer.tsx
@@ -40,15 +40,6 @@ const useStyles = makeStyles((theme: Theme) =>
       width: drawerWidth,
       flexShrink: 0,
       whiteSpace: 'nowrap',
-      '&::-webkit-scrollbar': {
-        width: '0.2em'
-      },
-      '&::-webkit-scrollbar-track': {
-        background: '#f1f1f1'
-      },
-      '&::-webkit-scrollbar-thumb': {
-        background: '#888' 
-      }
     },
     drawerOpen: {
       width: drawerWidth,
--- web/src/theme.tsx
+++ web/src/theme.tsx
@@ -15,6 +15,21 @@ const theme = createMuiTheme({
       paper: '#292929',
     }
   },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '*::-webkit-scrollbar': {
+          width: '0.7em'
+        },
+        '*::-webkit-scrollbar-track': {
+          background: '#303030'
+        },
+        '*::-webkit-scrollbar-thumb': {
+          background: '#6b6b6b' 
+        }
+      }
+    }
+  }
 });
 
 export default theme;
